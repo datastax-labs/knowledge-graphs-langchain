584b4eff2ffc4b169769635f8a297190fbedd22b
Ben Chambers <35960+bjchambers@users.noreply.github.com>
2024-04-09 12:29:02 -0700

    feat: Include type in node identity
    This required changing the `source IN (...)` query to use a bunch of
    `source_name = ? AND source_type = ?` queries in parallel. To reduce
    duplication and simplify the concurrency, changed `traverse` to use
    `atraverse`.
    
    - Include node type in the source/target of each edge.
    - Support querying based on specific types.
    
    TODO:
    
    - [ ] Move prompts / basic runnable chains into the library to
          simplify the notebook and enable testing.
    - [ ] Update the notebook to show usage

--- tests/test_traverse.py
+++ tests/test_traverse.py
@@ -8,95 +8,95 @@ from .conftest import DataFixture
 
 def test_traverse_marie_curie(marie_curie: DataFixture) -> None:
     results = traverse(
-        "Marie Curie",
+        start=("Marie Curie", "Person"),
         steps=1,
         edge_table=marie_curie.edge_table,
         session=marie_curie.session,
         keyspace=marie_curie.keyspace,
     )
     expected = {
-        Relation("Marie Curie", "Polish", "HAS_NATIONALITY"),
-        Relation("Marie Curie", "French", "HAS_NATIONALITY"),
-        Relation("Marie Curie", "Physicist", "HAS_PROFESSION"),
-        Relation("Marie Curie", "Chemist", "HAS_PROFESSION"),
-        Relation("Marie Curie", "Professor", "HAS_PROFESSION"),
-        Relation("Marie Curie", "Radioactivity", "RESEARCHED"),
-        Relation("Marie Curie", "Nobel Prize", "WON"),
-        Relation("Marie Curie", "Pierre Curie", "MARRIED_TO"),
-        Relation("Marie Curie", "University of Paris", "WORKED_AT"),
+        Relation("Marie Curie", "Person", "Polish", "Nationality", "HAS_NATIONALITY"),
+        Relation("Marie Curie", "Person", "French", "Nationality", "HAS_NATIONALITY"),
+        Relation("Marie Curie", "Person", "Physicist", "Profession", "HAS_PROFESSION"),
+        Relation("Marie Curie", "Person", "Chemist", "Profession", "HAS_PROFESSION"),
+        Relation("Marie Curie", "Person", "Professor", "Profession", "HAS_PROFESSION"),
+        Relation("Marie Curie", "Person", "Radioactivity", "Scientific concept", "RESEARCHED"),
+        Relation("Marie Curie", "Person", "Nobel Prize", "Award", "WON"),
+        Relation("Marie Curie", "Person", "Pierre Curie", "Person", "MARRIED_TO"),
+        Relation("Marie Curie", "Person", "University of Paris", "Organization", "WORKED_AT"),
     }
     assert_that(results, contains_exactly(*expected))
 
     results = traverse(
-        "Marie Curie",
+        start=("Marie Curie", "Person"),
         steps=2,
         edge_table=marie_curie.edge_table,
         session=marie_curie.session,
         keyspace=marie_curie.keyspace,
     )
-    expected.add(Relation("Pierre Curie", "Nobel Prize", "WON"))
+    expected.add(Relation("Pierre Curie", "Person", "Nobel Prize", "Award", "WON"))
     assert_that(results, contains_exactly(*expected))
 
 
 async def test_atraverse_marie_curie(marie_curie: DataFixture) -> None:
     results = await atraverse(
-        "Marie Curie",
+        start=("Marie Curie", "Person"),
         steps=1,
         edge_table=marie_curie.edge_table,
         session=marie_curie.session,
         keyspace=marie_curie.keyspace,
     )
     expected = {
-        Relation("Marie Curie", "Polish", "HAS_NATIONALITY"),
-        Relation("Marie Curie", "French", "HAS_NATIONALITY"),
-        Relation("Marie Curie", "Physicist", "HAS_PROFESSION"),
-        Relation("Marie Curie", "Chemist", "HAS_PROFESSION"),
-        Relation("Marie Curie", "Professor", "HAS_PROFESSION"),
-        Relation("Marie Curie", "Radioactivity", "RESEARCHED"),
-        Relation("Marie Curie", "Nobel Prize", "WON"),
-        Relation("Marie Curie", "Pierre Curie", "MARRIED_TO"),
-        Relation("Marie Curie", "University of Paris", "WORKED_AT"),
+        Relation("Marie Curie", "Person", "Polish", "Nationality", "HAS_NATIONALITY"),
+        Relation("Marie Curie", "Person", "French", "Nationality", "HAS_NATIONALITY"),
+        Relation("Marie Curie", "Person", "Physicist", "Profession", "HAS_PROFESSION"),
+        Relation("Marie Curie", "Person", "Chemist", "Profession", "HAS_PROFESSION"),
+        Relation("Marie Curie", "Person", "Professor", "Profession", "HAS_PROFESSION"),
+        Relation("Marie Curie", "Person", "Radioactivity", "Scientific concept", "RESEARCHED"),
+        Relation("Marie Curie", "Person", "Nobel Prize", "Award", "WON"),
+        Relation("Marie Curie", "Person", "Pierre Curie", "Person", "MARRIED_TO"),
+        Relation("Marie Curie", "Person", "University of Paris", "Organization", "WORKED_AT"),
     }
     assert_that(results, contains_exactly(*expected))
 
     results = await atraverse(
-        "Marie Curie",
+        start=("Marie Curie", "Person"),
         steps=2,
         edge_table=marie_curie.edge_table,
         session=marie_curie.session,
         keyspace=marie_curie.keyspace,
     )
-    expected.add(Relation("Pierre Curie", "Nobel Prize", "WON"))
+    expected.add(Relation("Pierre Curie", "Person", "Nobel Prize", "Award", "WON"))
     assert_that(results, contains_exactly(*expected))
 
-@pytest.mark.skip("filtering not yet supported")
+
 def test_traverse_marie_curie_filtered_edges(marie_curie: DataFixture) -> None:
     results = traverse(
-        "Marie Curie",
+        start=("Marie Curie", "Person"),
         steps=1,
-        edge_filters=["type = 'HAS_NATIONALITY'"],
+        edge_filters=["edge_type = 'HAS_NATIONALITY'"],
         edge_table=marie_curie.edge_table,
         session=marie_curie.session,
         keyspace=marie_curie.keyspace,
     )
     expected = {
-        Relation("Marie Curie", "Polish", "HAS_NATIONALITY"),
-        Relation("Marie Curie", "French", "HAS_NATIONALITY"),
+        Relation("Marie Curie", "Person", "Polish", "Nationality", "HAS_NATIONALITY"),
+        Relation("Marie Curie", "Person", "French", "Nationality", "HAS_NATIONALITY"),
     }
     assert_that(results, contains_exactly(*expected))
 
-@pytest.mark.skip("filtering not yet supported")
+
 async def test_atraverse_marie_curie_filtered_edges(marie_curie: DataFixture) -> None:
     results = await atraverse(
-        "Marie Curie",
+        start=("Marie Curie", "Person"),
         steps=1,
-        edge_filters=["type = 'HAS_NATIONALITY'"],
+        edge_filters=["edge_type = 'HAS_NATIONALITY'"],
         edge_table=marie_curie.edge_table,
         session=marie_curie.session,
         keyspace=marie_curie.keyspace,
     )
     expected = {
-        Relation("Marie Curie", "Polish", "HAS_NATIONALITY"),
-        Relation("Marie Curie", "French", "HAS_NATIONALITY"),
+        Relation("Marie Curie", "Person", "Polish", "Nationality", "HAS_NATIONALITY"),
+        Relation("Marie Curie", "Person", "French", "Nationality", "HAS_NATIONALITY"),
     }
     assert_that(results, contains_exactly(*expected))
