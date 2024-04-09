7c5f70fef19b9d279e63a6735c49f2f5f9705d44
Ben Chambers <35960+bjchambers@users.noreply.github.com>
2024-04-09 15:34:30 -0700

    add extractor

--- knowledge_graph/__init__.py
+++ knowledge_graph/__init__.py
@@ -1,3 +1,4 @@
 from .cassandra_graph_store import CassandraGraphStore
+from .runnables import extract_entities
 
-__all__ = ["CassandraGraphStore"]
+__all__ = ["CassandraGraphStore", "extract_entities"]
